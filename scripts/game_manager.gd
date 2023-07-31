990308ff9bd508ac0607ed9f915d26f31b13028f
Caden Morrison <64432691+caden-knight@users.noreply.github.com>
2023-07-31 15:58:22 -0600

    added npc
    follows character in a very ugly way

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,17 +3,22 @@ extends Node2D
 var suspects: Array[String] = ["Tyler", "Jax", "Caden", "Erin", "Gracie", "Stella"]
 var victims: Array[String] = suspects
 var locations: Array[String] = ["Kitchen", "Dining Room", "Billiard Room", "Study", "Conservatory", "Ballroom", "Hall", "Library"]
-var weapons: Array[String] = ["Gun", "Knife", "Rope", "Wrench", "Candlestick", "Leadpipe"]
+var weapons: Array[String] = ["Gun", "Knife", "Rope", "Wrench", "Candlestick", "Lead pipe"]
 
 func _ready():
 	game_setup()
 
 func game_setup():
 	var suspect: String = suspects.pick_random()
-	victims.erase(suspect)
 	var victim: String = victims.pick_random()
 	var location: String = locations.pick_random()
 	var weapon: String = weapons.pick_random()
+	
+	# remove the results from the arrays
+	suspects.erase(suspect)
+	victims.erase(suspect)
+	locations.erase(location)
+	weapons.erase(weapon)
 	
 	var solution_string = "%s was killed by %s with the %s in the %s"
 	var solution = solution_string % [victim,suspect, weapon, location]
